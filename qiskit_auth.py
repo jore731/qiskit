42a6151372ef9c3893cf8ae26000910ec8885d1f
Jorge Pulido <jorge.pulido@hotmail.com>
2024-02-24 02:38:20 +0000

    done

--- qiskit_auth.py
+++ qiskit_auth.py
@@ -1,5 +1,7 @@
 import os
 
 from qiskit_ibm_provider import IBMProvider
+from qiskit_ibm_runtime import QiskitRuntimeService
 
 IBMProvider.save_account(os.environ["IBMQ_TOKEN"], overwrite=True)
+QiskitRuntimeService.save_account(channel='ibm_quantum', token='my_token', overwrite=True)
